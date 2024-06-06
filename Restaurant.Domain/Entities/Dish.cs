ad3009ced574516dad905d48b75c8f996f49c5af
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-06-06 15:09:54 +0200

    Basic entities added

---
+++ Restaurant.Domain/Entities/Dish.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurant.Domain.Entities;
+
+public class Dish
+{
+	public Guid DishId { get; set; }
+
+	public string Name { get; set; } = default!;
+	public string Description { get; set; } = default!;
+	public decimal Price { get; set; }
+}
