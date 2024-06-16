57d21b6bd0e5d5d801f98f1d1728e3de4f2aa041
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-06-16 21:55:04 +0200

    Fluent Validation added

--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Application.Restaurants;
 
@@ -8,7 +10,10 @@ public static class ServiceCollectionExtensions
 {
 	public static void AddApplication(this IServiceCollection services)
 	{
+		Assembly applicationAssembly = typeof(ServiceCollectionExtensions).Assembly;
+		
 		services.AddScoped<IRestaurantsService, RestaurantsService>();
-		services.AddAutoMapper(typeof(ServiceCollectionExtensions).Assembly);
+		services.AddAutoMapper(applicationAssembly);
+		services.AddValidatorsFromAssembly(applicationAssembly).AddFluentValidationAutoValidation();
 	}
 }
--- Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
+++ Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
@@ -4,20 +4,15 @@ namespace Restaurants.Application.Restaurants.Dtos;
 
 public class CreateRestaurantDto
 {
-	[StringLength(100, MinimumLength = 3)]
 	public string Name { get; set; } = default!;
 	public string Description { get; set; } = default!;
-	[Required(ErrorMessage = "You need to insert a valid category")]
 	public string Category { get; set; } = default!;
 	public bool HasDelivery { get; set; }
-
-	[EmailAddress(ErrorMessage = "Please provide a valid email address")]
+	
 	public string? ContactEmail { get; set; }
-	[Phone(ErrorMessage = ("Please provide a valid phone number"))]
 	public string? ContactNumber { get; set; }
 	
 	public string? City { get; set; }
 	public string? Street { get; set; }
-	[RegularExpression(@"^\d{2}-\d{3}$", ErrorMessage = "Please provide a valid postal code (xx-xxx)")]
 	public string? PostalCode { get; set; }
 }
