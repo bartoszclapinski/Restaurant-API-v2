2d0fa2026230bdf421b90cc5a9e3eafaadfa296d
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-06-29 14:39:13 +0200

    Get dish and all dishes for restaurant added

---
+++ Restaurants.Application/Dishes/Queries/GetDishByIdForRestaurant/GetDishByIdForRestaurantQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Restaurants.Application.Dishes.Dtos;
+
+namespace Restaurants.Application.Dishes.Queries.GetDishByIdForRestaurant;
+
+public class GetDishByIdForRestaurantQuery(Guid restaurantId, Guid dishId) : IRequest<DishDto>
+{
+	public Guid RestaurantId { get; } = restaurantId;
+	public Guid DishId { get; } = dishId;
+}
---
+++ Restaurants.Application/Dishes/Queries/GetDishByIdForRestaurant/GetDishByIdForRestaurantQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Application.Dishes.Dtos;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Exceptions;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Application.Dishes.Queries.GetDishByIdForRestaurant;
+
+public class GetDishByIdForRestaurantQueryHandler(
+	ILogger<GetDishByIdForRestaurantQueryHandler> logger,
+	IMapper mapper,
+	IRestaurantRepository restaurantRepository) : IRequestHandler<GetDishByIdForRestaurantQuery, DishDto>
+{
+
+	public async Task<DishDto> Handle(GetDishByIdForRestaurantQuery request, CancellationToken cancellationToken)
+	{
+		logger.LogInformation("Getting dish for restaurant: {@RestaurantId} and dish: {@DishId}", request.RestaurantId, request.DishId);
+		Restaurant restaurant = await restaurantRepository.GetByIdAsync(request.RestaurantId) 
+		                        ?? throw new NotFoundException(nameof(Restaurant), request.RestaurantId.ToString());
+		Dish dish = restaurant.Dishes.FirstOrDefault(d => d.DishId == request.DishId) 
+		            ?? throw new NotFoundException(nameof(Dish), request.DishId.ToString());
+		var dishDto = mapper.Map<DishDto>(dish);
+		
+		return dishDto;
+	}
+}
