9503d978783a2db9964d43ec9dd722a0f91a7994
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-07-10 21:48:05 +0200

    Extending user claims

---
+++ Restaurants.Infrastructure/Authorization/RestaurantsUserClaimsPrincipalFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Authorization;
+
+public class RestaurantsUserClaimsPrincipalFactory(
+	UserManager<User> userManager,
+	RoleManager<IdentityRole> roleManager,
+	IOptions<IdentityOptions> options)
+	: UserClaimsPrincipalFactory<User, IdentityRole>(userManager, roleManager, options)
+{
+	public override async Task<ClaimsPrincipal> CreateAsync(User user)
+	{
+		ClaimsIdentity id = await GenerateClaimsAsync(user);
+
+		if (user.Nationality != null) id.AddClaim(new Claim("Nationality", user.Nationality));
+		if (user.DateOfBirth != null) id.AddClaim(new Claim("DateOfBirth", user.DateOfBirth.ToString("yyyy-MM--dd")));
+
+		return new ClaimsPrincipal(id);
+	}
+}
