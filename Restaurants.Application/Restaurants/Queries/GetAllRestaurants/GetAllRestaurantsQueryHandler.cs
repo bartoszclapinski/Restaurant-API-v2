b9e031628d287f09af21ff990fa9acbcef9f40a7
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-07-17 18:02:51 +0200

    Search functionality added

--- Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryHandler.cs
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryHandler.cs
@@ -15,7 +15,7 @@ public class GetAllRestaurantsQueryHandler(
 	public async Task<IEnumerable<RestaurantDto>> Handle(GetAllRestaurantsQuery request, CancellationToken cancellationToken)
 	{
 		logger.LogInformation("Getting all restaurants");
-		var restaurants = await restaurantRepository.GetAllAsync();
+		var restaurants = await restaurantRepository.GetAllMatchingAsync(request.SearchPhrase);
 		
 		return mapper.Map<IEnumerable<RestaurantDto>>(restaurants);
 	}
--- Restaurants.Domain/Repositories/IRestaurantRepository.cs
+++ Restaurants.Domain/Repositories/IRestaurantRepository.cs
@@ -5,6 +5,7 @@ namespace Restaurants.Domain.Repositories;
 public interface IRestaurantRepository
 {
 	Task<IEnumerable<Restaurant>> GetAllAsync();
+	Task<IEnumerable<Restaurant>> GetAllMatchingAsync(string? searchPhrase);
 	Task<Restaurant?> GetByIdAsync(Guid id);
 	Task<Guid> CreateAsync(Restaurant restaurant);
 	Task DeleteAsync(Restaurant restaurant);
