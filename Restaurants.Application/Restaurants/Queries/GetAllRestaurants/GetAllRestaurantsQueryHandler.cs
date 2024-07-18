ce499db7bfbc9c1b127eba6de8685232c64e25e2
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-07-18 19:07:44 +0200

    Results pagination setup

--- Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryHandler.cs
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryHandler.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Restaurants.Application.Common;
 using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Domain.Repositories;
 
@@ -9,14 +10,19 @@ namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 public class GetAllRestaurantsQueryHandler(
 	ILogger<GetAllRestaurantsQueryHandler> logger,
 	IMapper mapper,
-	IRestaurantRepository restaurantRepository) : IRequestHandler<GetAllRestaurantsQuery, IEnumerable<RestaurantDto>>
+	IRestaurantRepository restaurantRepository) : IRequestHandler<GetAllRestaurantsQuery, PagedResult<RestaurantDto>>
 {
 
-	public async Task<IEnumerable<RestaurantDto>> Handle(GetAllRestaurantsQuery request, CancellationToken cancellationToken)
+	public async Task<PagedResult<RestaurantDto>> Handle(GetAllRestaurantsQuery request, CancellationToken cancellationToken)
 	{
 		logger.LogInformation("Getting all restaurants");
-		var restaurants = await restaurantRepository.GetAllMatchingAsync(request.SearchPhrase);
+		var (restaurants, totalCount) = await restaurantRepository
+			.GetAllMatchingAsync(request.SearchPhrase, request.PageNumber, request.PageSize);
 		
-		return mapper.Map<IEnumerable<RestaurantDto>>(restaurants);
+		var restaurantDtos =  mapper.Map<IEnumerable<RestaurantDto>>(restaurants);
+		
+		var result = new PagedResult<RestaurantDto>(restaurantDtos, totalCount, request.PageSize, request.PageNumber);
+
+		return result;
 	}
 }
