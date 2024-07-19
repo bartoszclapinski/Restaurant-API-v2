f8cba4c6a7be321407f9cc087fc8b78c9de10345
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2024-07-19 16:21:42 +0200

    Pagination request validator

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+
+public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+	private int[] allowedPageSizes = [5, 10, 15, 20, 25, 30];
+	
+	public GetAllRestaurantsQueryValidator()
+	{
+		RuleFor(x => x.PageNumber).GreaterThan(1);
+		RuleFor(x => x.PageSize)
+			.Must(value => allowedPageSizes.Contains(value))
+			.WithMessage($"Page size must be one of the following: {string.Join(", ", allowedPageSizes)}");
+	}
+}
